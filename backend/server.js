7745d1cea47d534d67ddad5cf5d1171412248172
Darius Wilson <dariuswilsonxu@gmail.com>
2023-05-22 21:30:25 -0500

    Finish Productish

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 const port = 4000
+const cors = require('cors')
 
+app.use(cors());
 
 app.get('/teams', (req, res) => { // http://localhost:4000/teams
   fs.readFile('/Users/16303/Desktop/Projects/Project1/backend/team_player.json', 'utf8', (err, data) => {
