112f2e84ed7393241ec3c93714cff61411e3f500
Darius Wilson <dariuswilsonxu@gmail.com>
2025-11-06 17:01:17 -0600

    Fix: Update file path to use path.join with __dirname

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,19 @@ const fs = require("fs");
 const app = express();
 const port = 4000;
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
 
 app.get("/teams", (req, res) => {
   // http://localhost:4000/teams
-  fs.readFile(
-    "/Users/16303/Desktop/Projects/NBAPlayerList/backend/team_player.json",
-    "utf8",
-    (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      res.json(JSON.parse(data));
+  fs.readFile(path.join(__dirname, "team_player.json"), "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
     }
-  );
+    res.json(JSON.parse(data));
+  });
 });
 
 app.listen(port, () => {
